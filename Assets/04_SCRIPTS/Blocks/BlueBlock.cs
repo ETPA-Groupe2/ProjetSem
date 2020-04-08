d41a8eceadff84d1cd439b30e17f0914b4a3ebc7
Pauline B <pauline.barbet1@gmail.com>
2020-04-08 16:29:35 +0200

    Réactions de deux blocs entre eux en utilisant une interface
    Un bloc bleu se fait éjecter dans une direction lorsqu'il rentre en contact avec le bloc violet. Le bloc violet lorsqu'il a déjà été touché 1 fois est devenu jaune.

---
+++ Assets/04_SCRIPTS/Blocks/BlueBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueBlock : MonoBehaviour, IBlock1
+{
+    [SerializeField]
+    public Rigidbody m_blueBlockTR;
+
+    [SerializeField]
+    public float m_force;
+
+    private void Start()
+    {
+        m_blueBlockTR.GetComponent<Rigidbody>();
+       
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PurpleBlock")
+        {
+            onTouch();
+        }
+    }
+
+    public void onTouch()
+    {
+        m_blueBlockTR.AddForce((transform.position + new Vector3(0,0,-1)) * m_force);
+    }
+}
---
+++ Assets/04_SCRIPTS/Blocks/PurpleBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleBlock : MonoBehaviour, IBlock2
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "BlueBlock")
+        {
+            onTouch();
+        }
+    }
+
+    public void onTouch()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+}
