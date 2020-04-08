a5703745d06b0240cb57a259968592cac8ecbe94
Pauline B <pauline.barbet1@gmail.com>
2020-04-08 16:38:11 +0200

    Ajout de l'ennemi codé précédemment en package (Ennemi V1)

---
+++ Assets/04_SCRIPTS/Enemy/TriggerZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    [SerializeField]
+    private int m_attack;
+
+    [SerializeField]
+    public int m_attackMax;
+
+    [SerializeField]
+    public GameObject m_ennemi;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Zone")
+        {
+            m_ennemi.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            if(m_attack <= m_attackMax)
+            {
+                m_attack += 50;
+            }
+        }
+        
+        Debug.Log(m_attack);
+    }
+
+}
