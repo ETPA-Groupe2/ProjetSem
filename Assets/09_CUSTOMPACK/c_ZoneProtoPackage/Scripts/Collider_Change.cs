22fe58ac9748ea9a50c9d64a5dca9366c4f669e4
eldersys <62941832+eldersys@users.noreply.github.com>
2020-04-22 17:15:36 +0200

    Zone Fonctionne

--- Assets/09_CUSTOMPACK/c_ZoneProtoPackage/Scripts/Collider_Change.cs
+++ Assets/09_CUSTOMPACK/c_ZoneProtoPackage/Scripts/Collider_Change.cs
@@ -11,62 +11,96 @@ public class Collider_Change : MonoBehaviour
     SphereCollider m_collider;
     public GameObject m_sphere;
     // slowdown time
-    private float m_timeScale = 0.5f;
+    [SerializeField]private float m_timeSpeed;
     //increase scale's sphere
-    private Vector3 m_scaleChange;
+    [SerializeField] private Vector3 m_scaleChange;
     // decrease scale's sphere
-    private Vector3 m_scaleDown;
+    [SerializeField] private Vector3 m_initialScale;
     //limit increase/decrease scale of the sphere
-    private float m_sphereRad = 0.0f;
+    [SerializeField] private float m_sphereRad = 0.0f;
     //for energy value
     public Energy m_energy;
 
-    public bool m_isTrigger = false;
+    IEnumerator Lerp;
+    IEnumerator LerpBack;
+
+    [Tooltip("Check if we're triggering the zone")]public bool m_isTrigger = false;
 
     [Tooltip("Scaling of the collider.")] public float m_x, m_y, m_z;
+
+
 
 
     void Start()
     {
         // call the collider
         m_collider = GetComponent<SphereCollider>();
+        m_initialScale = transform.localScale;
     }
 
 
     public void OnTrigger()
     {
         m_isTrigger = true;
-        m_scaleChange = new Vector3(m_x, m_y, m_z);
-        //value for fix time
-        float progress = 0;
-        //increase scale of sphere
-        if(m_sphereRad<=8f && m_energy.m_energy>0)
-        {
-            Debug.Log("increasing");
-         //increment m_sphereRad for limit increase scale sphere
-            m_sphereRad++;
-            m_sphere.transform.localScale += m_scaleChange;
-           //slowing time of increase/decrease
-            progress += Time.deltaTime * m_timeScale;  
-        }
+
+        if(Lerp != null)
+            StopCoroutine(Lerp);
+        if(LerpBack != null)
+            StopCoroutine(LerpBack);
+
+        Lerp = LerpRoutine();
+
+        StartCoroutine(Lerp);
     }
 
     public void OffTrigger()
     {
         m_isTrigger = false;
-        m_scaleDown = new Vector3(-m_x, -m_y, -m_z);
-        //decrease scale of sphere
-        if (m_sphereRad >= 0.0f || m_energy.m_energy<=0)
+
+        if(LerpBack != null)
+            StopCoroutine(LerpBack);
+        if(Lerp != null)
+            StopCoroutine(Lerp);
+            
+        LerpBack = LerpBackRoutine();
+
+        StartCoroutine(LerpBack);
+    }
+
+    IEnumerator LerpRoutine()
+    {
+        m_scaleChange = new Vector3(m_x, m_y, m_z);
+
+        while(true)
         {
-        //fix decrease scale value
-            if(m_sphere.transform.localScale.x >= 2f && m_sphere.transform.localScale.y >= 2f && m_sphere.transform.localScale.z >= 2f)
+            m_sphere.transform.localScale = Vector3.Lerp(m_sphere.transform.localScale, m_scaleChange, Time.deltaTime*m_timeSpeed);
+
+            if(m_energy.m_energy>0)
             {
-                Debug.Log("reducing");
-        //decrement for reset value of m_sphereRad
-                m_sphereRad--;
-                m_sphere.transform.localScale += m_scaleDown;
+                yield return false;
+            }
+            else
+            {
+                yield return new WaitForEndOfFrame();
             }
         }
+    }
+
+    IEnumerator LerpBackRoutine()
+    {
+       while(true)
+        {
+            m_sphere.transform.localScale = Vector3.Lerp(m_sphere.transform.localScale, m_initialScale, Time.deltaTime*m_timeSpeed);
+
+            if(m_energy.m_energy<=0)
+            {
+                yield return false;
+            }
+            else
+            {
+                yield return new WaitForEndOfFrame();
+            }
+        } 
     }
 
 }
