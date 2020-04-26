77661e6a8ffc891c44c04c18ffec3c4ed3fe0424
Molotovv <X6jojo@hotmail.fr>
2020-04-26 18:35:15 +0200

    Scene Change
    Change scene between HUB and other areas

---
+++ Assets/09_CUSTOMPACK/c_ChangeScene/Scripts/ChangeArea.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Manages the switch between all the areas
+/// </summary>
+public class ChangeArea : MonoBehaviour
+{
+    [Header("Scenes names")]
+
+    [Tooltip("Name of the Hub scene")]
+    [SerializeField] string m_HUBScene;
+
+    [Tooltip("Name of the Area 1 scene")]
+    [SerializeField] string m_Area1Scene;
+
+    [Tooltip("Name of the Area 2 scene")]
+    [SerializeField] string m_Area2Scene;
+
+    [Tooltip("Name of the Area 3 scene")]
+    [SerializeField] string m_Area3Scene;
+
+    //Loads the area 1
+    public void GoToArea1 ()
+    {
+        SceneManager.LoadScene(m_Area1Scene);
+    }
+
+    // Loads the area 2
+    public void GoToArea2()
+    {
+        SceneManager.LoadScene(m_Area2Scene);
+    }
+
+    // Load the area 3
+    public void GoToArea3()
+    {
+        SceneManager.LoadScene(m_Area3Scene);
+    }
+
+    // Loads the Hub
+    public void GoToHUB()
+    {
+        SceneManager.LoadScene(m_HUBScene);
+    }
+}
