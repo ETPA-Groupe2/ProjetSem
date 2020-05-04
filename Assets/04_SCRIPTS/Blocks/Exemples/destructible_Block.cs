c7184a5d7843e1da9187c80b4811dcc320b7e6d6
Krahg-Row <rpoussier@hotmail.com>
2020-05-04 16:11:24 +0200

    Box destructible
    La galère frère

---
+++ Assets/04_SCRIPTS/Blocks/Exemples/destructible_Block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GD2Lib;
+
+public class destructible_Block : Blocks
+{
+    [SerializeField] private Animator m_anim;
+    [SerializeField] private GameObject m_Ressources;
+
+    public override void handleReaction(GD2Lib.Event p_event, object[] p_params)
+    {
+       
+        m_anim.Play("cube_destroy");
+        Debug.Log("yakalelo yakalelo");
+        Instantiate(m_Ressources, transform.position, transform.rotation);
+           
+        
+    }
+
+}
