307a2754b705c20e6cc33be1ec318325386a27cc
Molotovv <X6jojo@hotmail.fr>
2020-05-05 11:17:52 +0200

    Pressure button
    Adds the pressure button with choosable event to play when activated

---
+++ Assets/09_CUSTOMPACK/c_PressureButton/Scripts/Pressure.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GD2Lib;
+/// <summary>
+/// Manages the detection of the objects with the pressure button
+/// </summary>
+public class Pressure : MonoBehaviour
+{
+
+
+    [Header("Event")]
+    [Tooltip("The event you want to play when the button is pressed")]
+    [SerializeField] GD2Lib.Event m_event;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        m_event.Raise();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
