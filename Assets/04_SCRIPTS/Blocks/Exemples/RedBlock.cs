6d3ac5e17379a8370db6f29eabb6794e9c1ed83b
Krahg-Row <rpoussier@hotmail.com>
2020-05-05 17:38:21 +0200

    LD 3.2
    restes plus qu'a mettre les interrupteurs + placement de cube

--- Assets/04_SCRIPTS/Blocks/Exemples/RedBlock.cs
+++ Assets/04_SCRIPTS/Blocks/Exemples/RedBlock.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using GD2Lib;
 
-public class RedBlock : Blocks
+public class RedBlock : Blocks , IExplosion
 {
     [SerializeField] GameObject m_explosionFx;
     private bool m_exploded = false;
     [SerializeField]private float m_blastRadius = 5f;
+
+
+    
 
     public override void handleReaction(GD2Lib.Event p_event, object[] p_params)
     {
@@ -22,15 +25,28 @@ public class RedBlock : Blocks
         Collider[] colliders = Physics.OverlapSphere(transform.position, m_blastRadius);
         foreach (Collider nearbyObject in colliders)
         {
-            if(nearbyObject.TryGetComponent<Animator>(out Animator anim))
+            IExplosion e = nearbyObject.GetComponent<IExplosion>();
+
+            if (nearbyObject.TryGetComponent<Animator>(out Animator anim))
             {
+                
                 anim.Play("explosion_anim");
+            }
+            if(e != null)
+            {
+                e.onTouch();
             }
         }
 
         Destroy(gameObject);
         m_exploded = true;
 
+    }
+
+    public void onTouch()
+    {
+        if (m_event != null)
+            m_event.Raise();
     }
 
 }
---
+++ Assets/04_SCRIPTS/Interfaces/IExplosion.cs
@@ -0,0 +1,5 @@
+﻿
+ public interface IExplosion 
+{
+    void onTouch();
+}
