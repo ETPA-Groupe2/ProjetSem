cc5ddb0326104b0e7135771326d5b5dca7708598
Krahg-Row <rpoussier@hotmail.com>
2020-05-11 16:49:43 +0200

    Dissolve_shader

---
+++ Assets/04_SCRIPTS/Dissolve_Effect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve_Effect : MonoBehaviour
+{
+
+    [SerializeField] Material[] m_mat;
+    [SerializeField] Animator m_anim;
+    private Renderer m_rend;
+
+    
+    void Start()
+    {
+        m_rend = GetComponent<Renderer>();
+        m_rend.enabled = true;
+        m_rend.sharedMaterial = m_mat[0];
+    }
+
+    void Update()
+    {
+        if(this.m_anim.GetCurrentAnimatorStateInfo(0).IsName("explosion_anim"))
+        {
+            m_rend.sharedMaterial = m_mat[1];
+            Invoke("destroyGO", 3f);
+        }
+    }
+
+    public void destroyGO()
+    {
+        Destroy(gameObject);
+    }
+}
