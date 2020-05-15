47c61113e2c87589be4a2fa492d9c5793be0c116
Molotovv <X6jojo@hotmail.fr>
2020-05-15 09:50:55 +0200

    Final rating display + minor fix
    Adds the final rating display at the end of the level and the endscreen, and the fix of the pollution level

---
+++ Assets/04_SCRIPTS/EndScreen/Rating.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This class manages the final rate display
+/// </summary>
+public class Rating : MonoBehaviour
+{
+    [Header("Script management")]
+    [Tooltip("Put the PollutionRate script here")]
+    [SerializeField] private PollutionRate m_pollutionRate;
+
+    [Header("Display management")]
+    [Tooltip("Put here the text box in which you want to display the final rating")]
+    [SerializeField] private Text m_textDisplayer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            m_pollutionRate.FinalRating();
+            m_textDisplayer.text = "Vous êtes du rang " + m_pollutionRate.m_rateString + " !";
+            Invoke("LoadEndScene", 4f);
+        }
+    }
+
+    //Temporary
+    void LoadEndScene()
+    {
+        SceneManager.LoadScene("End");
+    }
+}
