58f415654e17dd0e79333c0d73d2f97c0909a52b
Molotovv <X6jojo@hotmail.fr>
2020-05-25 18:02:42 +0200

    wip tv screen effect
    wip tv screen effect

---
+++ Assets/04_SCRIPTS/Screens/CheckCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour
+{
+    [HideInInspector]
+    public bool m_isTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Zone"))
+        {
+            m_isTriggered = true;
+            Debug.Log("TRIGGERED");
+        }
+    }
+}
---
+++ Assets/04_SCRIPTS/Screens/Multimat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Multimat : MonoBehaviour
+{
+    [SerializeField]
+    public int CurrentMaterial;
+
+    [SerializeField]
+    public Material[] Materials;
+    // Use this for initialization
+
+    [Tooltip("Put the Zone detector here")]
+    [SerializeField] CheckCollision m_bool;
+
+    bool m_matHasChanged = false;
+    bool m_valueChanged = false;
+
+    void Start()
+    {
+        Renderer rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (m_bool.m_isTriggered == true)
+        {
+            Debug.Log("IN MULTIMAT");
+            if (CurrentMaterial == Materials.Length && m_matHasChanged == false)
+            {
+                CurrentMaterial = 0;
+            }
+            else if (m_valueChanged == false)
+            {
+                m_matHasChanged = true;
+                m_valueChanged = true;
+                CurrentMaterial++;
+            }
+
+            Material[] currentlyAssignedMaterials = GetComponent<Renderer>().materials;
+
+            currentlyAssignedMaterials[1] = Materials[CurrentMaterial];
+            GetComponent<Renderer>().materials = currentlyAssignedMaterials;
+        }
+    }
+}
